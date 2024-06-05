de98465e904f9506890eb1e129941ea475acfa31
Super Student Knur Campa <student@knur.club>
2024-06-05 12:12:48 +0000

    feat: create a serverSocket

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <sys/socket.h>
 
 int main(void)
 {
     printf("http-server has started\n");
+
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+
+    printf("http-server has closed\n");
     return 0;
 }
